0a8cbea48ef9e3e955fd342d589125f4ec645d13
Barath-K-R <barath223637@gmail.com>
2024-09-17 12:04:24 +0530

    file handling

---
+++ src/main/java/org/example/Input.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.io.*;
+public class Input {
+    static void input() {
+
+        //InputStreamReader
+        try (InputStreamReader isr = new InputStreamReader(System.in)) {
+            System.out.print("Enter some letters:");
+            int letters = isr.read();
+            while(isr.ready()) {
+                System.out.println((char) letters);
+                letters = isr.read();
+            }
+            // isr.close();
+            System.out.println();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // FileReader
+        try (FileReader fr = new FileReader("note.txt")) {
+            int letters = fr.read();
+            while(fr.ready()) {
+                System.out.println((char)letters);
+                letters = fr.read();
+            }
+            // fr.close();
+            System.out.println();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // byte to char stream and then reading char stream
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("You typed: " + br.readLine());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader("note.txt"))) {
+            while (br.ready()) {
+                System.out.println(br.readLine());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
